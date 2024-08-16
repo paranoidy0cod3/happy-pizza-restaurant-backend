0987c9f2660cd3ab45e32c78fa38af066c792d76
paranoidy0cod3 <35105531+paranoidy0cod3@users.noreply.github.com>
2024-08-16 14:00:41 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_DOMAIN,
+    credentials: true,
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
+    allowedHeaders: "Authorization, Content-Type",
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
